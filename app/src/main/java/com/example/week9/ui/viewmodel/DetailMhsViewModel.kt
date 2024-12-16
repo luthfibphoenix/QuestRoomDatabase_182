dee45ae92efc882f4899bc5b02c114a506fd50c8
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-16 12:01:30 +0700

    membuat data class DetailUiState

--- app/src/main/java/com/example/week9/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/week9/ui/viewmodel/DetailMhsViewModel.kt
@@ -3,6 +3,21 @@ package com.example.week9.ui.viewmodel
 import com.example.week9.data.entity.Mahasiswa
 
 
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoadings: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+
 fun Mahasiswa.toDetailUiEvent() : MahasiswaEvent {
     return MahasiswaEvent(
         nim = nim,
