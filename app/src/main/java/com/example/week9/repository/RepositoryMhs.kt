216a61a03bee66a3936cb6bc82a7cb81e869085e
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-16 10:35:45 +0700

    membuat funciton pada interface pada RepositoryMhs

--- app/src/main/java/com/example/week9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/week9/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.week9.repository
 
 import com.example.week9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahassiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
