e41578f5ff66c861ccdf49172631bb8527ed8a38
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-16 12:27:03 +0700

    melengkapi pada PenyediaViewModel

--- app/src/main/java/com/example/week9/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/week9/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.week9.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
@@ -13,6 +14,23 @@ object PenyediaViewModel {
         initializer {
             MahasiswaViewModel(
                 KrsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            HomeMhsViewModel(
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs,
             )
         }
     }
