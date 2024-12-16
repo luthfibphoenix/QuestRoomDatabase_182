0225f9ba64df462057f8115a367b88a21e5aaadf
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-16 10:34:28 +0700

    membuat fungsi pada LocalRepositoryMhs

--- app/src/main/java/com/example/week9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/week9/repository/LocalRepositoryMhs.kt
@@ -2,12 +2,29 @@ package com.example.week9.repository
 
 import com.example.week9.data.dao.MahasiswaDao
 import com.example.week9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ): RepositoryMhs {
     override suspend fun insertMhs(mahassiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahassiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 
 }
