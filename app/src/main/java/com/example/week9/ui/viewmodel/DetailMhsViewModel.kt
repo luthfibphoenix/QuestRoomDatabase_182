2c5d5f62d167c88c23e849c6d8c20536aefaba28
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-16 12:13:12 +0700

    melengkapi yang eror pada class DetailMhsViewModel

--- app/src/main/java/com/example/week9/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/week9/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,12 +1,70 @@
 package com.example.week9.ui.viewmodel
 
-import com.example.week9.data.entity.Mahasiswa
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.week9.repository.RepositoryMhs
+import com.example.week9.ui.navigation.DestinationDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+): ViewModel(){
+    private val _nim: String = checkNotNull(savedStateHandle[DestinationDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit (DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+    fun deleteMhs(){
+        detailUiState.value.detailUiEvent.nim.let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
+}
 
 
 
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
-    val isLoadings: Boolean = false,
+    val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
 ){
