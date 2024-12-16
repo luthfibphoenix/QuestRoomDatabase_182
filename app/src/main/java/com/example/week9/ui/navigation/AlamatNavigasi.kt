e0818d08591322f88545c1e68ddd77a67ae3edb3
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-16 10:43:26 +0700

    membuat object pada AlamatNavigasi

--- app/src/main/java/com/example/week9/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/week9/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,19 @@ package com.example.week9.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object Destination : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinationDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routeWithArgs = "$route/{$NIM}"
+}
+
+object DestinationUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routeWithArgs = "$route/{$NIM}"
+}
